0451eb2aa14b2341baaa03371a8f8558804435ad
Claire Nord <Claire Nord>
2015-11-21 20:21:07 -0500

    First commit

--- wilhacksproject.java
+++
@@ -1,8 +0,0 @@
-public class WildHacks{
-	String word = 
-	String url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=35463c47-6c93-48f4-b839-abc34fa8866a";
-String charset = "UTF-8";
-String param1 = "value1";
-	
-
-}
